dabd9dae2ea5c4d28c792b2101466ac3e6290f2c
Markus Kahl <machisuji@googlemail.com>
2010-05-29 11:09:03 +0200

    cleaned 'workspace'

--- framework/src/java/wandledi/java/Workspace.java
+++ framework/src/java/wandledi/java/Workspace.java
@@ -14,7 +14,7 @@ public class Workspace {
 
     public static void main(String[] args) throws Exception {
 
-        wandle();
+        match(".info", "p.info");
     }
 
     public static void match(String s1, String s2) {
@@ -22,28 +22,5 @@ public class Workspace {
         Selector a = Selector.valueOf(s1);
         Selector b = Selector.valueOf(s2);
         System.out.println(s1 + " = " + s2 + ": " + a.equals(b));
-    }
-
-    private static void wandle() throws IOException {
-
-        WorkPages pages = new WorkPages();
-        pages.index("22:43");
-        Scroll scroll = pages.getScroll();
-        Wandler wandler = new Wandler();
-        String template = "/home/markus/Entwicklung/java/Wandledi/application/web/WEB-INF/view/home2/index.xhtml";
-        String output = "/home/markus/Desktop/index.html";
-        wandler.useScroll(scroll);
-        wandler.wandle(new FileReader(template), new FileWriter(output));
-    }
-}
-
-class WorkPages extends Pages {
-
-    public void index(String time) {
-
-        get("#time").replace(true, time);
-        get(".info").insertLast(" Wandledi is great!");
-        get("#time").setAttribute("style", "color: red;");
-        get("div").setAttribute("style", "font-weight: bold;");
     }
 }
