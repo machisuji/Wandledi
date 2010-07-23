76b426179b05e4078745c169d2276106c16c3695
Markus Kahl <machisuji@googlemail.com>
2010-07-23 12:44:05 +0200

    sbt through and through. Need to adjust packaging.

--- scala-lib/project/build/build.scala
+++
@@ -1,9 +0,0 @@
-import sbt._
-
-class WandlediScala(projectInfo: ProjectInfo) extends DefaultProject(projectInfo) {
-
-  lazy val desc = task {
-    println("Wandledi Scala is a Scala wrapper for the Wandledi Java API.")
-    None
-  }
-}
