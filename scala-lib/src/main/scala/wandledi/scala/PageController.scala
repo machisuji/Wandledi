b7728c5a2be40ee311f41447eda62ec776406177
Markus Kahl <machisuji@googlemail.com>
2011-01-25 23:30:17 +0100

    Removed web stuff from scala-lib.

--- scala-lib/src/main/scala/wandledi/scala/PageController.scala
+++
@@ -1,8 +0,0 @@
-package wandledi.scala
-
-import wandledi.scala.html.Page
-
-abstract class PageController extends Controller with wandledi.java.SpellController {
-  override def isSpellController = true
-  def getPage: Page
-}
