baa8b4fd37f57104f77647b13ef7ffbb04739e1a
Markus Kahl <machisuji@googlemail.com>
2011-09-08 21:14:23 +0200

    generate one scaladoc for all projects

--- project/build/wandledi.scala
+++ project/build/wandledi.scala
@@ -7,7 +7,17 @@ class Wandledi(info: ProjectInfo) extends ParentProject(info) {
   lazy val wandlet = project("wandlet", "wandlet", new Wandlet(_), core)
   lazy val wandletScala = project("wandlet-scala", "wandlet-scala", new WandletScala(_),
         scalaLib, wandlet)
-  
+  lazy val docs = project("docs", "wandledi-docs", new Docs(_), wandletScala)
+
+  def concatPaths[T](s: Seq[T])(f: PartialFunction[T, PathFinder]): PathFinder = {
+    def finder: T => PathFinder = f orElse { case _ => Path.emptyPathFinder }
+    (Path.emptyPathFinder /: s)(_ +++ finder(_))
+  }
+
+  def deepSources = concatPaths(topologicalSort) { case p: ScalaPaths => p.mainSources }
+  def deepDocClasspath = (Path.emptyPathFinder /: dependencies.map {
+    case p: DefaultProject => p.compileClasspath })(_ +++ _)
+
   class Core(info: ProjectInfo) extends DefaultProject(info) {
     val htmlparser = "nu.validator.htmlparser" % "htmlparser" % "1.2.1" % "compile"
     val testng = "org.testng" % "testng" % "5.12.1" % "test" // for Java only
@@ -15,9 +25,13 @@ class Wandledi(info: ProjectInfo) extends ParentProject(info) {
     val scalatest = "org.scalatest" % "scalatest" % "1.2.1-SNAPSHOT" % "test"
   }
   class ScalaLib(info: ProjectInfo) extends DefaultProject(info)
-  
+
   class Wandlet(info: ProjectInfo) extends DefaultProject(info) {
     val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided"
   }
   class WandletScala(info: ProjectInfo) extends DefaultProject(info)
+
+  class Docs(info: ProjectInfo) extends DefaultProject(info) {
+    lazy val docs = scaladocTask("wandledi", deepSources, docPath, deepDocClasspath, documentOptions)
+  }
 }
