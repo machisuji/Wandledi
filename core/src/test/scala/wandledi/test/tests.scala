b5742c1e1b2c7e6f4a1b3d0d6c6ff3d08b91cc69
Markus Kahl <machisuji@googlemail.com>
2011-09-14 23:18:05 +0200

    that doesn't belong here

--- core/src/test/scala/wandledi/test/tests.scala
+++ core/src/test/scala/wandledi/test/tests.scala
@@ -8,5 +8,3 @@ class SelectorExperimentWrapper extends SelectorExperiment with TestNGSuite
 
 class HTML5Wrapper extends HTML5 with TestNGSuite
 class HtmlSpellExperiment extends SpellExperiment(Wandler.forHTML) with TestNGSuite // test everything again with HtmlParser just to be sure
-
-class StaxToSaxAdapterTestWrapper extends StaxToSaxAdapterTest with TestNGSuite
