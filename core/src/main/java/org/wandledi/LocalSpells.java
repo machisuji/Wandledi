a4b8c5cf39951ba6d165579c773cb234e2aba1b3
Markus Kahl <machisuji@googlemail.com>
2011-10-11 19:33:15 +0200

    got rid of some newlines

--- core/src/main/java/org/wandledi/LocalSpells.java
+++ core/src/main/java/org/wandledi/LocalSpells.java
@@ -12,26 +12,22 @@ public class LocalSpells extends AbstractSpell {
     private Scroll localScroll;
 
     public LocalSpells(Scroll parentScroll, Scroll localScroll) {
-
         this.parentScroll = parentScroll;
         this.localScroll = localScroll;
     }
 
     public void startTransformedElement(String label, Attributes attributes) {
-
         super.startTransformedElement(label, attributes);
         parentScroll.addScroll(localScroll);
     }
 
     public void endTransformedElement(String label) {
-
         parentScroll.removeScroll(localScroll);
         super.endTransformedElement(label);
     }
 
     @Override
     public Spell clone() {
-
         return new LocalSpells(parentScroll, localScroll);
     }
 }
